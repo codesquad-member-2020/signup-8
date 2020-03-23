f8c82a8240c0cca5e89f5c510b9098418c7d6741
blossun <dnsngu@gmail.com>
2020-03-23 20:32:27 +0900

    [Chore] 프로젝트 초기화
    Spring Initializr - Spring Web, Spring Data JDBC, Spring Boot DevTools, H2 Database
    gitignore 설정

---
+++ be/signup08/src/main/java/com/codesquad/signup08/Signup08Application.java
@@ -0,0 +1,13 @@
+package com.codesquad.signup08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Signup08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Signup08Application.class, args);
+	}
+
+}
