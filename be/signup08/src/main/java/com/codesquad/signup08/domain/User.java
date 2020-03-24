1afbef41bdcc6c453c9feb47e8f4f96fa2908190
blossun <dnsngu@gmail.com>
2020-03-24 22:18:14 +0900

    [Feat] 데이터베이스에 User 객체 정보 저장
    User의 birthday 데이터 타입을 잘못 정의해서 수정함.
    LocalDateTime → LocalDate

--- be/signup08/src/main/java/com/codesquad/signup08/domain/User.java
+++ be/signup08/src/main/java/com/codesquad/signup08/domain/User.java
@@ -2,7 +2,7 @@ package com.codesquad.signup08.domain;
 
 import org.springframework.data.annotation.Id;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.Arrays;
 
 public class User {
@@ -11,12 +11,24 @@ public class User {
     private String userId;
     private String password;
     private String name;
-    private LocalDateTime birthday;
+    private LocalDate birthday;
     private String gender;
     private String email;
     private String phoneNumber;
     private String[] interest;
-    private boolean agreement;
+    private String agreement;
+
+    public User(String userId, String password, String name, LocalDate birthday, String gender, String email, String phoneNumber, String[] interest, String agreement) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.interest = interest;
+        this.agreement = agreement;
+    }
 
     @Override
     public String toString() {
---
+++ be/signup08/src/main/resources/user-data.sql
@@ -0,0 +1,2 @@
+INSERT INTO USER(USER_ID, PASSWORD, NAME, BIRTHDAY, GENDER, EMAIL, PHONE_NUMBER, INTEREST, AGREEMENT) VALUES ('solar', '1111', 'solari', '2020-03-24', 'female', 'solar@test.com', '010-1111-2222', 'board, movie', 'true');
+INSERT INTO USER(USER_ID, PASSWORD, NAME, BIRTHDAY, GENDER, EMAIL, PHONE_NUMBER, INTEREST, AGREEMENT) VALUES ('holar', '1234', 'solari', '2020-03-24', 'male', 'holar@test.com', '010-1111-2222', 'board, movie', 'true');
