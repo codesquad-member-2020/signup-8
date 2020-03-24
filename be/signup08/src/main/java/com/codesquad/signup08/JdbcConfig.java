dbdf543d40d995918932ecdb98773d7721b90716
blossun <dnsngu@gmail.com>
2020-03-24 15:56:24 +0900

    [Feat] JDBC 연동을 위한 기본 설정
    To do
    * user-schema.sql : User 테이블 스키마 쿼리 작성
    * user-data.sql : User 테이블에 테스트용 계정 추가

---
+++ be/signup08/src/main/java/com/codesquad/signup08/JdbcConfig.java
@@ -0,0 +1,38 @@
+package com.codesquad.signup08;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJdbcRepositories
+public class JdbcConfig extends AbstractJdbcConfiguration {
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .generateUniqueName(true)
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("user-schema.sql")
+                .addScript("user-data.sql")
+                .build();
+    }
+}
