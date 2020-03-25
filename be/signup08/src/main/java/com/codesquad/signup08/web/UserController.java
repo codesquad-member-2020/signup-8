d1fb5bd4e14bb2e2c54de6a8d380fca09c19803e
blossun <dnsngu@gmail.com>
2020-03-25 15:48:47 +0900

    [Feat] 회원가입 실패 시 에러처리
    save하는 순간 발생하는 에러 처리
    * 잘못된 컬럼명
    * ENUM 컬럼에 잘못된 값이 들어온 경우
    //To Do
    잘못된 날짜 형식으로 들어온 경우 유효성 체크나 에러처리 필요

--- be/signup08/src/main/java/com/codesquad/signup08/web/UserController.java
+++ be/signup08/src/main/java/com/codesquad/signup08/web/UserController.java
@@ -2,12 +2,15 @@ package com.codesquad.signup08.web;
 
 import com.codesquad.signup08.domain.User;
 import com.codesquad.signup08.domain.UserRepository;
+import com.codesquad.signup08.exception.NotFoundUserException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.relational.core.conversion.DbActionExecutionException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -32,6 +35,15 @@ public class UserController {
         log.debug("UserId : {}", user);
         userRepository.save(user);
         return "redirect:/";
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    private String catchBadRequestException(Model model, DbActionExecutionException e) {
+        log.debug("[*] {}", e.getMessage());
+        final String ERROR_MESSAGE = "잘못된 요청입니다.";
+        model.addAttribute("errorMessage", ERROR_MESSAGE);
+        return "/error";
     }
 
 }
