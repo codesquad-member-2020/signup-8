7b08b1cb5e9b48163487c49baace0f67d1257e70
1Consumption <hanseop95@gmail.com>
2020-03-25 22:08:04 +0900

    [Fix] 테두리 색상 변경 방법 수정
    노티피케이션을 관찰하다가 감지가 되면 텍스트 필드의 테두리 색상 변경
    또한 UI를 수정하기 때문에 main스레드에서 작업함.

--- iOS/Signup/Signup/View/SignupTextField.swift
+++ iOS/Signup/Signup/View/SignupTextField.swift
@@ -16,25 +16,59 @@ class SignUpTextField: UITextField {
         case Red
     }
     
-    var borderColor: BorderColor {
+    private var borderColor: BorderColor {
         didSet {
             changeBorderColor(color: borderColor)
         }
     }
     
     private func changeBorderColor(color: BorderColor) {
-        setBorder(color: UIColor(named: color.rawValue) ?? .gray, width: 1)
+        DispatchQueue.main.async {
+            if self.isEditing {
+                if self.text?.count ?? 0 < 5 {
+                    self.setBorder(color: UIColor(named: "Red") ?? .red , width: 1)
+                    NotificationCenter.default.post(name: .isValidID,
+                                                    object: nil,
+                                                    userInfo: ["labelState" : StatusLabel.State.ShortLength])
+                } else if self.text?.count ?? 0 > 20{
+                    self.setBorder(color: UIColor(named: "Red") ?? .red , width: 1)
+                    NotificationCenter.default.post(name: .isValidID,
+                                                    object: nil,
+                                                    userInfo: ["labelState" : StatusLabel.State.LongLength])
+                } else {
+                    self.setBorder(color: UIColor(named: color.rawValue) ?? .gray, width: 1)
+                }
+            }
+        }
     }
     
     override init(frame: CGRect) {
         self.borderColor = .Gray
         super.init(frame: frame)
         setBorder(color: UIColor(named: borderColor.rawValue) ?? .gray, width: 1)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(changeBorderColor(_ :)),
+                                               name: .borderColor,
+                                               object: nil)
     }
     
     required init?(coder: NSCoder) {
         self.borderColor = .Gray
         super.init(coder: coder)
         setBorder(color: UIColor(named: borderColor.rawValue) ?? .gray, width: 1)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(changeBorderColor(_ :)),
+                                               name: .borderColor,
+                                               object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func changeBorderColor(_ notification: Notification) {
+        if let info: [String : BorderColor] = notification.userInfo as? [String : BorderColor] {
+            changeBorderColor(color: info["borderColor"] ?? .Red)
+        }
     }
 }
