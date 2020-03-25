8dd9e97647a6361cc60aff491eead678547b9a27
1Consumption <hanseop95@gmail.com>
2020-03-25 17:10:07 +0900

    [Feat] network 클래스 추가
    서버와 통신하여 제이슨 객체를 받아 옴

---
+++ iOS/Signup/Signup/Model/NetworkHandler.swift
@@ -0,0 +1,45 @@
+//
+//  NetworkHandler.swift
+//  Signup
+//
+//  Created by 신한섭 on 2020/03/25.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import Foundation
+
+class NetworkHandler {
+    class func request(resource: String){
+        let defaultSession = URLSession(configuration: .default)
+        guard let url = URL(string: resource) else {
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        let dataTask = defaultSession.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+            guard error == nil else {
+                print("Error occur: \(String(describing: error))")
+                
+                return
+            }
+            
+            guard let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                return
+            }
+        
+            guard let jsonToArray = try? JSONSerialization.jsonObject(with: data, options: []) else {
+                print("json to Any Error")
+                
+                return
+            }
+            print(jsonToArray)
+            guard let json = jsonToArray as? [String:Bool] else {return}
+            if let status = json["isValid"] {
+                print(status)
+            }
+        }
+        
+        dataTask.resume()
+    }
+}
