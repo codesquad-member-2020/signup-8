fe146f6106022407e060658b34b738bf0c3c8ae2
choisohyun <sohyun4283@naver.com>
2020-03-26 16:43:58 +0900

    [Feat] #8 유효성 검사 skeleton 코드 작성

---
+++ FE/js/signup/validation.js
@@ -0,0 +1,32 @@
+const checkId = () => {};
+
+const isValidId = () => {};
+
+const isUniqueId = () => {};
+
+const checkPswd1 = () => {};
+
+const checkPswd2 = () => {};
+
+const isEqualPswd = () => {};
+
+const checkBirth = () => {};
+
+const isValidBirth = () => {};
+
+const checkEmail = () => {};
+
+const isValidEmail = () => {};
+
+const checkPhoneNo = () => {};
+
+const isValidPhoneNo = () => {};
+
+const checkFormInfos = () => {
+  checkId();
+  checkPswd1();
+  checkPswd2();
+  checkBirth();
+  checkEmail();
+  checkPhoneNo();
+};
