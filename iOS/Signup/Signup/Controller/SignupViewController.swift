a26a608ab62c0dbb3ff5ffb489e95d6d328455b8
1Consumption <hanseop95@gmail.com>
2020-03-27 11:22:43 +0900

    [Fix] ViewController 이름 변경
    ViewController가 여러개 생성됨에 따라 이름을 구분지어줌

--- iOS/Signup/Signup/Controller/SignupViewController.swift
+++ iOS/Signup/Signup/Controller/SignupViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignupViewController: UIViewController {
 
     @IBOutlet weak var IDTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
