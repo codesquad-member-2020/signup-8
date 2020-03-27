f25293f773110986e91846d29e65cda074d48b95
1Consumption <hanseop95@gmail.com>
2020-03-27 15:01:50 +0900

    [Feat] TermsViewController 추가
    약관 화면을 표시를 담당 함

---
+++ iOS/Signup/Signup/Controller/TermsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TermsViewController.swift
+//  Signup
+//
+//  Created by 신한섭 on 2020/03/27.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import UIKit
+
+class TermsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
