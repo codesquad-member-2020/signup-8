b159b01d3bbf5553437dd47ca04da622ce24328d
1Consumption <hanseop95@gmail.com>
2020-03-27 11:23:51 +0900

    [Feat] PersonalInformationViewController 생성 및 view 생성

---
+++ iOS/Signup/Signup/Controller/PersonalInformationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PersonalInformationViewController.swift
+//  Signup
+//
+//  Created by 신한섭 on 2020/03/27.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import UIKit
+
+class PersonalInformationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
