f44d0fff4ad2a235e10bb4788ac2743bed1ef4ef
1Consumption <hanseop95@gmail.com>
2020-03-27 11:50:07 +0900

    [Feat] 화면 전환 기능 추가
    SignupViewController에서 PersonalInformationViewController로 전환

--- iOS/Signup/Signup/Controller/SignupViewController.swift
+++ iOS/Signup/Signup/Controller/SignupViewController.swift
@@ -9,13 +9,20 @@
 import UIKit
 
 class SignupViewController: UIViewController {
-
+    
     @IBOutlet weak var IDTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var passwordConfirmTextField: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet var labels: [UILabel]!
+    
+    @IBAction func nextButtonPushed(_ sender: UIButton) {
+        let personalViewController = self.storyboard!.instantiateViewController(identifier: "PersonalInformationViewController")
+        personalViewController.modalPresentationStyle = .fullScreen
+        present(personalViewController, animated: true)
+    }
+    
     
     private let idTextFieldDelegate = IDTextFieldDelegate()
     
