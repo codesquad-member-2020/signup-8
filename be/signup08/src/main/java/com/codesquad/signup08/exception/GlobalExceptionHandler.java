151288cd2cfca7c85a867fc272110b27edcca04f
blossun <dnsngu@gmail.com>
2020-03-27 16:52:24 +0900

    [Feat] 잘못된 요청에 대한 Response Status 추가

--- be/signup08/src/main/java/com/codesquad/signup08/exception/GlobalExceptionHandler.java
+++ be/signup08/src/main/java/com/codesquad/signup08/exception/GlobalExceptionHandler.java
@@ -11,16 +11,19 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundUserException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public @ResponseBody ResponseResult handleUserNotFoundException(NotFoundUserException e) {
         return new ResponseResult(false, e.getMessage());
     }
 
     @ExceptionHandler(IncorrectInputException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public @ResponseBody ResponseResult handleIncorrectInputException(IncorrectInputException e) {
         return new ResponseResult(false, e.getMessage());
     }
 
     @ExceptionHandler(DbActionExecutionException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public @ResponseBody ResponseResult catchBadRequestException(DbActionExecutionException e) {
         final String BAD_REQUEST_MESSAGE = "잘못된 요청입니다.";
         return new ResponseResult(false, BAD_REQUEST_MESSAGE);
