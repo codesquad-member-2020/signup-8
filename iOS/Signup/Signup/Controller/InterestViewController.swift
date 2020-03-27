c96b0e088f9c04165e381b90b70a4feb326888cf
1Consumption <hanseop95@gmail.com>
2020-03-27 16:04:18 +0900

    [Feat] InterestViewController 추가
    관심사를 표시 할 view를 관리 및 표시함

---
+++ iOS/Signup/Signup/Controller/InterestViewController.swift
@@ -0,0 +1,16 @@
+//
+//  InterestViewController.swift
+//  Signup
+//
+//  Created by 신한섭 on 2020/03/27.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import UIKit
+
+class InterestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
